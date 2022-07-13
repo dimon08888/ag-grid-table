97772c6ac7106987bf5431cb122ee10e281d12ea
dimon08888 <dimon08888@mail.ru>
2022-07-13 22:57:21 +0300

    Добавлена таблица на ag-grid-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { AgGridReact } from 'ag-grid-react'
+import 'ag-grid-community/dist/styles/ag-grid.css'
+import 'ag-grid-community/dist/styles/ag-theme-alpine-dark.css'
+import { useState, useEffect, useMemo, useCallback, useRef } from 'react'
 
 function App() {
+  const [rowData, setRowData] = useState([
+    { make: 'Ford', model: 'Focus', price: 40000 },
+    { make: 'Toyota', model: 'Camry', price: 140000 },
+    { make: 'BMW', model: '4 series', price: 240000 },
+  ])
+
+  const gridRef = useRef()
+
+  const [columnDefs, setColumnsDefs] = useState([
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' },
+  ])
+
+  const defaultColDef = useMemo(
+    () => ({
+      sortable: true,
+      filter: true,
+      resizable: true,
+    }),
+    []
+  )
+
+  const cellClickedListener = useCallback(e => {
+    console.log('cellClicked', e)
+  }, [])
+
+  // useEffect(() => {
+  //   fetch('https://www.ag-grid.com/example-assets/row-data.js')
+  //     .then(result => result.json())
+  //     .then(rowData => setRowData(rowData))
+  // }, [])
+
+  const pushMeClicked = useCallback(e => {
+    gridRef.current.ref.api.deselectAll()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='ag-theme-alpine-dark' style={{ height: 500 }}>
+      <button onClick={pushMeClicked}>Push Me</button>
+      <AgGridReact
+        ref={gridRef}
+        onCellClicked={cellClickedListener}
+        rowData={rowData}
+        columnDefs={columnDefs}
+        rowSelection='multiple'
+        animateRows={true}
+        defaultColDef={defaultColDef}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
